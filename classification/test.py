73b85d2fd2b305d16f6dadbbf6bf2ce5cfda329d
Martin Schicklgruber <martin.schicklgruber@solvistas.com>
2021-02-25 11:28:42 +0100

    Ned Dataloader

--- classification/test.py
+++ classification/test.py
@@ -7,39 +7,16 @@ import torch
 import sys
 import os
 
+from torch.utils.data import DataLoader
 from tqdm import tqdm
+
+from data.basic_dataset import BasicDataset
+from data.transforms import create_basic_transforms
 
 sys.path.append(os.getcwd())
 
 from detect_from_video import get_boundingbox, predict_with_model
-from network.models import model_selection
-from src.util.data.data_loader import load_subset
-from src.util.dotdict import Dotdict
 from src.util.validate import calc_scores
-
-
-def get_opt():
-    opt = Dotdict()
-
-    opt.model = 'all'
-    opt.is_train = True
-    opt.pretrained = True
-    opt.checkpoints_dir = './out/checkpoints/faces'
-    opt.continue_train = True
-    opt.save_name = 'latest'
-    opt.name = 'knn'
-    # opt.dataset_path = './datasets/celeb-df-v2/images'
-    opt.dataset_path = './datasets/forensic/scaled'
-    opt.multiclass = False
-    opt.resize_interpolation = 'bilinear'
-    opt.load_size = -1
-    opt.train_split = 'train'
-    opt.train_size = 2500
-    opt.val_split = 'val'
-    opt.val_size = 100
-    opt.test_split = 'test'
-
-    return opt
 
 
 def pred_image(model, face_detector, image, cuda):
@@ -63,38 +40,55 @@ def pred_image(model, face_detector, image, cuda):
         return prediction
 
 
-def test(model_path, cuda):
+def test(args):
     gc.collect()
     torch.cuda.empty_cache()
     # model, *_ = model_selection(modelname='xception', num_out_classes=2)
-    print(cuda)
-    if model_path is not None:
-        model = torch.load(model_path)
-        print('Model found in {}'.format(model_path))
-    else:
-        print('No model found, initializing random model.')
-    if cuda:
+    print(args.cuda)
+    model = torch.load(args.model_path)
+    print('Model found in {}'.format(args.model_path))
+
+    if args.cuda:
         model = model.cuda()
     face_detector = dlib.get_frontal_face_detector()
-    opt = get_opt()
-    test_img, test_label = load_subset(opt, opt.test_split, opt.load_size)
+
+    test_data = BasicDataset(root_dir=args.root_dir,
+                             processed_dir=args.processed_dir,
+                             crops_dir=args.crops_dir,
+                             split_csv=args.split_csv,
+                             seed=args.seed,
+                             normalize=None,
+                             transforms=create_basic_transforms(args.size),
+                             mode='test')
+    test_loader = DataLoader(test_data, batch_size=1)
+
     y_pred, y_true = [], []
-    for img, label in tqdm(zip(test_img, test_label), total=len(test_img)):
-        pred = pred_image(model, face_detector, img, cuda)
+    for img, label in tqdm(test_loader):
+        pred = pred_image(model, face_detector, img.numpy(), args.cuda)
         if type(pred) == int:
             y_pred.append(pred)
             y_true.append(label)
 
     acc, ap, auc = calc_scores(y_true, y_pred)[:3]
-    print("Detection only worked on {} from {} Images".format(len(y_pred), len(test_label)))
+    print("Detection only worked on {} from {} Images".format(len(y_pred), len(test_loader)))
     print("Test: acc: {}; ap: {}; auc: {}".format(acc, ap, auc))
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Parameters for Training")
+    args = parser.add_argument
+    # Dataset Options
+    args("--root_dir", default='datasets/dfdc', help="root directory")
+    args('--processed_dir', default='processed', help='directory where the processed files are stored')
+    args('--crops_dir', default='crops', help='directory of the crops')
+    args('--split_csv', default='folds.csv', help='Split CSV Filename')
+    args('--seed', default=111, help='Random Seed')
+
+    args('--model_path', '-mi', type=str,
+         default='./src/baselines/face_forensics/classification/models/face_detection/xception/all_c23.p')
+    args('--cuda', type=bool, default=True)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    p.add_argument('--model_path', '-mi', type=str,
-                   default='./src/baselines/face_forensics/classification/models/face_detection/xception/all_c23.p')
-    p.add_argument('--cuda', type=bool, default=True)
-    args = p.parse_args()
-    test(**vars(args))
+    test(parse_args())
